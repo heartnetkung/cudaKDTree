8ffbd2cfaac8582ff11e39e37cafd02bffc78474
heartnetkung <heartnetkung@gmail.com>
2023-08-30 16:52:13 +0100

    try dumb constant

--- testing/float3-knn.cu
+++ testing/float3-knn.cu
@@ -89,11 +89,11 @@ void knn500(float *d_results,
   d_knn500<<<nb,bs>>>(d_results,d_queries,numQueries,d_nodes,numNodes,maxRadius);
 }
 
-Float20 *readPoints(const char* file, int N)
+Float20 *readPoints(int N)
 {
   std::cout << "hello 1.1";
   using namespace cukd::common;
-  FILE* stream = fopen(file, "r");
+  FILE* stream = fopen("hello.txt", "r");
   char line[100];
   Float20 *d_points = 0;
   CUKD_CUDA_CALL(MallocManaged((void**)&d_points,N*sizeof(Float20)));
@@ -151,7 +151,7 @@ int main(int ac, const char **av)
   float maxQueryRadius = std::numeric_limits<float>::infinity();
   size_t nQueries = 10*1000*1000;
   int nRepeats = 1;
-  char* file=NULL;
+  int isAssigned = 0;
 
   for (int i=1;i<ac;i++) {
     std::string arg = av[i];
@@ -165,7 +165,7 @@ int main(int ac, const char **av)
       maxQueryRadius = std::stof(av[++i]);
     else if (arg == "-t"){
       std::cout << "hello 0";
-      file = "hello.txt";
+      isAssigned = 1;
       i++;
       std::cout << "hello 0.1";
     }
@@ -175,10 +175,10 @@ int main(int ac, const char **av)
   
   std::cout << "hello1";
   Float20 *d_points;
-  if(file == NULL){
+  if(isAssigned == 0){
     d_points = generatePoints(nPoints);
   }else{
-    d_points = readPoints(file, nPoints);
+    d_points = readPoints(nPoints);
   }
   std::cout << "hello2";
 
