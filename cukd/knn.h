e11af6999ebc08879923135f26c3e1209e2c5fd4
heartnetkung <heartnetkung@gmail.com>
2023-08-31 15:24:25 +0100

    fcl4

--- cukd/knn.h
+++ cukd/knn.h
@@ -212,7 +212,7 @@ namespace cukd {
         // the root ... while means we're done.
 
         // return maxRadius2;
-        return currentlyClosest.decode_pointID(currentlyClosest.entry[3]);
+        return currentlyClosest.decode_pointID(currentlyClosest.entry[4]);
     
       prev = curr;
       curr = next;
