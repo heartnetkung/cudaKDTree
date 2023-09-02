794ba4177ab473e47f8c395c9455efc6a6ce3b1c
heartnetkung <heartnetkung@gmail.com>
2023-09-02 17:59:07 +0100

    implement postprocessing and writeFile

--- testing/read_write.h
+++ testing/read_write.h
@@ -4,6 +4,7 @@
 #include "cukd/builder.h"
 #include "trie.h"
 #include "util.h"
+#include "leven1.h"
 
 struct FileContent{
 	Float20* d_points;
@@ -48,4 +49,47 @@ FileContent readContent(int n){
 	return ans;
 }
 
+std::vector<int> postprocessing(Float20* d_results, FileContent content, int n, int nResult){
+	std::vector<int> ans, indices;
+	TrieNode* root = content.str_queries_index;
+	char** str_queries = content.str_queries;
+	int* str_len_queries = content.str_len_queries;
+	TrieNode* result;
+	char* str1, str2;
+	int strLen1, strLen2, index2;
 
+	for(int j=0;j<nQueries;j++){
+		str1 = str_queries[j];
+		strLen1 = str_len_queries[j];
+
+		for(int k=0;k<nResult;k++){
+			int index = d_results[j*nResult+k];
+			if(index == -1)
+				continue;
+
+			result = search_trie(root,float2intarr(d_points[index]));
+			indices = result->payload;
+
+			for(int i=0;i<indices.size();i++){
+				index2 = indices.at(i);
+				if(index2==j)
+					continue;
+
+				str2 = str_queries[index2];
+				strLen2 = str_len_queries[index2];
+				if(leven1(str1,strLen1,str2,strLen2)==1){
+					ans.push_back(j);
+					ans.push_back(index2);
+				}
+			}
+		}
+	}
+	return ans;
+}
+
+void writeFile(std::vector<int> finalResult){
+	FILE* stream = fopen("output.txt", "w");
+	for(int i=0;i<finalResult.size();i+=2)
+		fprintf(stream,"%d %d",finalResult.at(i),finalResult.at(i+1));
+	fclose(stream);
+}
