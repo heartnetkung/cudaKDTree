430fa2bf5667cac6ac924b885abb87434e6214ab
heartnetkung <heartnetkung@gmail.com>
2023-09-02 14:18:34 +0100

    test trie

---
+++ testing/trie.h
@@ -0,0 +1,40 @@
+/**
+    Code for https://journaldev.com article
+    Purpose: A Trie Data Structure Implementation in C
+    @author: Vijay Ramachandran
+    @date: 20-02-2020
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <vector>
+
+// The number of children for each node
+// We will construct a N-ary tree and make it
+// a Trie
+// Since we have at most 10 repeated amino acids
+#define N 10
+
+struct TrieNode {
+    int data;
+    TrieNode* children[N];
+    int is_leaf;
+    vector<char*> payload;
+    vector<int> lengthOfPayload;
+};
+
+TrieNode make_trienode(int data) {
+    // Allocate memory for a TrieNode
+    TrieNode node;
+    for (int i=0; i<N; i++)
+        node.children[i] = NULL;
+    node.is_leaf = 0;
+    node.data=data;
+    return node;
+}
+
+void test_trie(){
+    TrieNode node = make_trienode(5);
+    std::cout << node.data << "c \n";
+}
